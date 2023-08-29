a2dc2434ba5d614d0595f46bd314061181d0f23d
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2023-08-29 21:09:57 +0400

    post factory created

---
+++ core/blog/factory.py
@@ -0,0 +1,27 @@
+import factory
+from django.contrib.auth import get_user_model
+from factory.faker import faker
+
+from .models import Post
+
+User = get_user_model()
+FAKE = faker.Faker()
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = factory.Faker('sentence', nb_words=12)
+    subtitle = factory.Faker('sentence', nb_words=12)
+    slug = factory.Faker('slug')
+    author = User.objects.get_or_create(username='admin')[0]
+
+    @factory.lazy_attribute
+    def content(self):
+        x = ''
+        for _ in range(5):
+            x += '\n' + FAKE.paragraph(nb_sentences=30) + '\n'
+        return x
+
+    status = 'published'
